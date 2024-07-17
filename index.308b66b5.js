5f9d425246d453c1e9174e091f58227e28c258e1
Polina <polinabondar4@gmail.com>
2024-07-18 01:05:20 +0300

    make build

---
+++ index.308b66b5.js
@@ -0,0 +1,2 @@
+!function(e,n){e.append(function e(n){var t=document.createElement("ul");for(var r in n){var a=document.createElement("li");a.textContent=r,t.append(a),Object.keys(n[r]).length>0&&a.append(e(n[r]))}return t}(n))}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.308b66b5.js.map
